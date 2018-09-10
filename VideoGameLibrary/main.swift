c26568330cea4f90f0f187aac6f5c899bd88191c
Tanner York <tanneryork@MacBook-Pro-4.local>
2018-09-10 12:45:13 -0500

    Commiting Initial Files

---
+++ VideoGameLibrary/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  VideoGameLibrary
+//
+//  Created by Tanner York on 9/10/18.
+//  Copyright © 2018 Tanner York. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
